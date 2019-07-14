ba817dbf28162185ebf860d08b36e5d8067ff09a
Matt Greer <matt.e.greer@gmail.com>
2019-07-14 16:55:23 -0700

    background: move game down to metadata

--- src/components/backgroundHeader.tsx
+++ src/components/backgroundHeader.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
 import classnames from "classnames";
-import { gamePath } from "../util/gamePath";
 
 import styles from "./backgroundHeader.module.css";
 
 interface BackgroundHeaderProps {
     className?: string;
     levelName: string;
-    gameNameUsa: string;
 }
 
 export const BackgroundHeader: React.FunctionComponent<BackgroundHeaderProps> = ({
@@ -18,12 +16,5 @@ export const BackgroundHeader: React.FunctionComponent<BackgroundHeaderProps> = 
 }) => {
     const classes = classnames(styles.root, className);
 
-    return (
-        <header className={classes}>
-            <div className={styles.levelName}>{levelName}</div>
-            <div className={styles.gameName}>
-                <Link to={gamePath(gameNameUsa)}>{gameNameUsa}</Link>
-            </div>
-        </header>
-    );
+    return <header className={classes}>{levelName}</header>;
 };
--- src/components/backgroundMetaData.tsx
+++ src/components/backgroundMetaData.tsx
@@ -3,21 +3,34 @@ import { Link } from "gatsby";
 import classnames from "classnames";
 import { systemPath } from "../util/systemPath";
 import { yearPath } from "../util/yearPath";
+import { gamePath } from "../util/gamePath";
 
 import styles from "./backgroundMetaData.module.css";
 
 interface BackgroundMetaDataProps {
     className?: string;
+    gameNameUsa: string;
     system: string;
     year?: string;
 }
 
-export const BackgroundMetaData: React.FunctionComponent<BackgroundMetaDataProps> = ({ className, system, year }) => {
+export const BackgroundMetaData: React.FunctionComponent<BackgroundMetaDataProps> = ({
+    className,
+    gameNameUsa,
+    system,
+    year,
+}) => {
     const classes = classnames(styles.root, className);
 
     return (
         <table className={classes}>
             <tbody>
+                <tr>
+                    <td>game</td>
+                    <td>
+                        <Link to={gamePath(gameNameUsa)}>{gameNameUsa}</Link>
+                    </td>
+                </tr>
                 <tr>
                     <td>system</td>
                     <td>
