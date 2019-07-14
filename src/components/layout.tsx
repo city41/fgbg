3bd110d2ecd0a2f74c68a06142e5b505b87eddec
Matt Greer <matt.e.greer@gmail.com>
2019-07-13 22:46:17 -0700

    global styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,6 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
-import "./layout.css";
 
 const Layout: React.FunctionComponent = ({ children }) => {
     const data = useStaticQuery(graphql`
