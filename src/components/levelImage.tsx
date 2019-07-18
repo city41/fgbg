5c69cad5816d722af655a4266f5f06806277161b
Matt Greer <matt.e.greer@gmail.com>
2019-07-18 06:25:52 -0700

    LevelImage: aspect ratio done entirely in css

--- src/components/levelImage.tsx
+++ src/components/levelImage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import classnames from "classnames";
 
 import styles from "./levelImage.module.css";
@@ -14,56 +14,13 @@ interface LevelImageProps {
 export const LevelImage: React.FunctionComponent<LevelImageProps> = ({ className, width, height, src, alt }) => {
     const classes = classnames(styles.root, className);
 
-    const [loading, setLoading] = useState(false);
-    const [hadError, setHadError] = useState(false);
-    const [windowWidth, setWindowWidth] = useState(0);
+    // const [loading, setLoading] = useState(false);
+    // const [hadError, setHadError] = useState(false);
 
     const actualImageWidth = parseInt(width);
     const actualImageHeight = parseInt(height);
     const imageAspectRatio = actualImageHeight / actualImageWidth;
+    const paddingTop = `${Math.floor(imageAspectRatio * 100)}%`;
 
-    const imageWidth = windowWidth < 401 ? Math.floor(windowWidth * 0.96) : Math.floor(windowWidth * 0.75);
-    const imageHeight = Math.floor(imageWidth * imageAspectRatio);
-
-    const dimensions = { width: imageWidth, height: imageHeight };
-
-    useEffect(() => {
-        function onWindowResize() {
-            setWindowWidth(window.innerWidth);
-        }
-        window.addEventListener("resize", onWindowResize);
-
-        return () => window.removeEventListener("resize", onWindowResize);
-    });
-
-    useEffect(() => {
-        setWindowWidth(window.innerWidth);
-    }, []);
-
-    return (
-        <div className={classes} style={dimensions}>
-            {hadError ? (
-                <>
-                    <div className={styles.errorContainer}>an error occurred, try reloading the page</div>
-                    <img />
-                </>
-            ) : loading ? (
-                <>
-                    <div className={styles.loadingContainer}>loading&hellip;</div>
-                    <img
-                        width="1"
-                        height="1"
-                        src={src}
-                        onLoad={() => setLoading(false)}
-                        onError={() => setHadError(true)}
-                    />
-                </>
-            ) : (
-                <>
-                    <div />
-                    <img {...dimensions} src={src} alt={alt} />
-                </>
-            )}
-        </div>
-    );
+    return <div className={classes} style={{ paddingTop, backgroundImage: `url(${src})` }} />;
 };
