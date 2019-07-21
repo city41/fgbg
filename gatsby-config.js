a19428ff7cf55ee7ad90b26f6f1cb97b4fb68a91
Matt Greer <matt.e.greer@gmail.com>
2019-07-21 15:27:38 -0700

    gatsby-config: fix manifest values

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: "Fighting Game Backgrounds",
+                short_name: "FGBG",
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+                icon: "src/images/faviconSrc.png",
             },
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
