c71350eb5bee6c57904b68392166d966fb3e81aa
Matt Greer <matt.e.greer@gmail.com>
2019-07-22 18:35:43 -0700

    404: somewhat passable

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-
+import { Link } from "gatsby";
 import { Layout } from "../components/layout";
 import SEO from "../components/seo";
 
+import styles from "./404.module.css";
+
 const NotFoundPage: React.FunctionComponent = () => (
     <Layout>
-        <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div className={styles.root}>
+            <SEO title="404: Not found" />
+            <h1>NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <Link to="/">head to the home page and try again</Link>
+        </div>
     </Layout>
 );
 
